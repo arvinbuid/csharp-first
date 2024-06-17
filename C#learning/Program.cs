bb82eac302de7db4a42503063ed60bc09f3e9057
Arvin Buid <arvinbuid01@gmail.com>
2024-06-18 01:54:28 +0800

    Add project files.

---
+++ C#learning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LearningCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Math class
+
+            Console.WriteLine("Math class");
+
+            Console.ReadKey();
+        }
+    }
+}
