1e907a11fac4eb5682ad9bc3f94c9763d8483e39
Arvin Buid <arvinbuid01@gmail.com>
2024-06-18 11:30:00 +0800

    Nested loops

--- C#learning/Program.cs
+++ C#learning/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace LearningCSharp
 {
@@ -6,9 +7,27 @@ namespace LearningCSharp
     {
         static void Main(string[] args)
         {
-            // Math class
+            // nested loops
 
-            Console.WriteLine("Math class");
+            Console.WriteLine("How many rows?: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many columns?: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What symbol?: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
 
             Console.ReadKey();
         }
